96daec292fbcade69febb9ebde135ccd67999090
poliyka <jkny3586@gmail.com>
2021-09-04 14:27:52 +0800

    add yarn

--- install.sh
+++ install.sh
@@ -9,8 +9,18 @@
 # Make a new file:
 ################################################################################
 
+#--------------------------------------------------
+# OE Settings
+#--------------------------------------------------
 OE_USER="poliyka"
 OE_HOME="/home/$OE_USER"
+
+#--------------------------------------------------
+# Choose Dependencise
+#--------------------------------------------------
+INSTALL_RANGER="True"
+INSTALL_NVM="True"
+INSTALL_YARN="True"
 
 #--------------------------------------------------
 # Update Server
@@ -33,24 +43,39 @@ OE_HOME="/home/$OE_USER"
 #--------------------------------------------------
 # Install Ranger
 #--------------------------------------------------
-# echo -e "\n---- Install Ranger ----"
-# sudo git clone https://github.com/ranger/ranger.git $OE_HOME/ranger
-# sudo su - -c "cd ${OE_HOME}/ranger; make install"
-# sudo su $OE_USER -c "ranger --copy-config=all"
-# sudo rm -rf ${OE_HOME}/ranger
+# if [ $INSTALL_RANGER = "True" ]; then
+#   echo -e "\n---- Install Ranger ----"
+#   sudo git clone https://github.com/ranger/ranger.git $OE_HOME/ranger
+#   sudo su - -c "cd ${OE_HOME}/ranger; make install"
+#   sudo su $OE_USER -c "ranger --copy-config=all"
+#   sudo rm -rf ${OE_HOME}/ranger
 
-# echo -e "\n---- Set Ranger open on vscode command ----"
-# sed -i -e '$a\
-# class code(Command):\
-# \tdef execute(self):\
-# \t\tdirname = self.fm.thisdir.path\
-# \t\tcodecmd = ["code", dirname]\
-# \t\tself.fm.execute_command(codecmd)\
-# ' $OE_HOME/.config/ranger/commands.py
+#   echo -e "\n---- Set Ranger open on vscode command ----"
+#   sed -i -e '$a\
+#   class code(Command):\
+#   \tdef execute(self):\
+#   \t\tdirname = self.fm.thisdir.path\
+#   \t\tcodecmd = ["code", dirname]\
+#   \t\tself.fm.execute_command(codecmd)\
+#   ' $OE_HOME/.config/ranger/commands.py
+# fi
 
 #--------------------------------------------------
 # Install Nvm + Yarn
 #--------------------------------------------------
-# echo -e "\n---- Install Nvm Mnager ----"
-# sudo su $OE_USER -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash"
-# sudo su $OE_USER -c "source $OE_HOME/.bashrc"
+# if [ $INSTALL_NVM = "True" ]; then
+#   echo -e "\n---- Install Nvm Mnager ----"
+#   sudo su $OE_USER -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash"
+# fi
+
+if [ $INSTALL_YARN = "True" ]; then
+  echo -e "\n---- Install Yarn ----"
+  sudo su $OE_USER -c "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key"
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  sudo apt-get update && sudo apt-get install yarn
+fi
+
+echo "-----------------------------------------------------------"
+echo "Done! Ubuntu Initialize Dependenies:"
+echo "Nginx configuration file: /etc/nginx/sites-available/odoo"
+echo "-----------------------------------------------------------"
