a8137a4e160083efcd9e6bd1679b395df470e94c
poliyka <jkny3586@gmail.com>
2021-09-04 15:34:03 +0800

    fin bashrc

--- install.sh
+++ install.sh
@@ -93,9 +93,9 @@ CONFIG_BASHRC="True"
 if [ $CONFIG_BASHRC = "True" ]; then
   sed -i -e '$a\
 parse_git_branch() {\
-\tgit branch 2> /dev/null | sed -e \"/^[^*]/d\\" -e \"s/* \(.*\)/ (\\1)/\"\
+  git branch 2> /dev/null | sed -e \"/^[^*]/d\" -e \"s/* \(.*\)/ (\\1)/\"\
 }\
-export PS1=\"\\[\\033[01;32m\\]\\u@\\h\\[\\e[91m\\]\\$(parse_git_branch) \\[\\e[1;33m\\]\\D{%Y/%m/%d} \\t\\[\\033[00m\\]:\\n\\[\\e[34m\\]\\w\\[\\e[00m\\]\$ \"
+export PS1=\"\\[\\033[01;32m\\]\\u@\\h \\[\\e[91m\\]\\$(parse_git_branch) \\[\\e[1;33m\\]\\D{%Y/%m/%d} \\t\\[\\033[00m\\]:\\n\\[\\e[34m\\]\\w\\[\\e[00m\\]\$ \"
 ' $OE_HOME/.bashrc
 fi
 
