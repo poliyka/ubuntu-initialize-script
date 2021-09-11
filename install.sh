c1860be6b2c9c3cbf90765ae02d5323a153688e4
poliyka <58025341+poliyka@users.noreply.github.com>
2021-09-11 23:35:48 +0800

    fix .bashrc git color space

--- install.sh
+++ install.sh
@@ -93,9 +93,9 @@ if [ $CONFIG_BASHRC_COLOR = "True" ]; then
   echo -e "\n---- Config bashrc color ----"
   sed -i -e '$a\
 parse_git_branch() {\
-  git branch 2> /dev/null | sed -e \"/^[^*]/d\" -e \"s/* \\(.*\\)/(\\1)/\"\
+  git branch 2> /dev/null | sed -e \"/^[^*]/d\" -e \"s/* \\(.*\\)/ (\\1)/\"\
 }\
-export PS1=\"\\[\\033[01;32m\\]\\u@\\h \\[\\e[91m\\]\\$(parse_git_branch) \\[\\e[1;33m\\]\\D{%Y/%m/%d} \\t\\[\\033[00m\\]:\\n\\[\\e[34m\\]\\w\\[\\e[00m\\]\$ \"
+export PS1=\"\\[\\033[01;32m\\]\\u@\\h\\[\\e[91m\\]\\$(parse_git_branch) \\[\\e[1;33m\\]\\D{%Y/%m/%d} \\t\\[\\033[00m\\]:\\n\\[\\e[34m\\]\\w\\[\\e[00m\\]\$ \"
 ' $OE_HOME/.bashrc
 fi
 
