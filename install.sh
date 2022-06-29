9a2e52f681d26e9d4831c6409637ccde1433dc9e
poliyka <58025341+poliyka@users.noreply.github.com>
2022-06-29 16:54:15 +0800

    feat(apt): install htop

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ if [ $INSTALL_DEPENDEN = "True" ]; then
   sudo apt-get install git python3 python3-pip build-essential wget make vim python3-dev -y
   sudo apt-get install libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl llvm libncurses5-dev -y
   sudo apt-get install libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl libxml2-dev -y
-  sudo apt-get install libxslt1-dev libjpeg62-dev vim-gtk3 pipenv silversearcher-ag exuberant-ctags figlet tox net-tools -y
+  sudo apt-get install libxslt1-dev libjpeg62-dev vim-gtk3 pipenv silversearcher-ag exuberant-ctags figlet tox net-tools htop -y
 fi
 
 #--------------------------------------------------
